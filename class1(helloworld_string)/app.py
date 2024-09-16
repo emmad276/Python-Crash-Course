43bf776479ba9715ebb9cafaf61156e1e5e1f317
emmad276 <emmadahmed90@gmail.com>
2024-09-17 03:48:03 +0500

    String and Other Data types

--- class1(helloworld_string)/app.py
+++ class1(helloworld_string)/app.py
@@ -1,2 +1,16 @@
-print("Hello World")
-print("Hello World")
+# print("Hello World")
+# print("Hello World")
+
+
+                                    # No Type Hint
+# name = "Emmad"
+# name= 700                 no error becaue it is a dynamic language
+# print(name)
+# print(type(name))
+# print(id(name))
+
+
+
+                                 # Restrict Data Type----------> Type Hint
+name: str = "Emmad"
+print(name)
