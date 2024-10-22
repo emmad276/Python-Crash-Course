2c28afc0b1774e1548cdba24f20ddfeb1a853a3d
emmad276 <emmadahmed90@gmail.com>
2024-10-22 18:40:14 +0500

    dictionary

---
+++ class7(dictionary)/app.py
@@ -0,0 +1,11 @@
+from typing import Dict, Union, Optional
+
+# create custom type
+Key = Union[int, str] 
+Value = Union[str,int,list, dict, set]
+
+data : dict[Key, Value] = {
+    "name": "Syed Emmad",
+    "fname": "Syed Tanveer",
+    "age": "30"
+}
