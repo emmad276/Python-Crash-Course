d05290efdc6c2d372e508244b733047b8f059808
emmad276 <emmadahmed90@gmail.com>
2024-11-03 14:36:28 +0500

    loop

---
+++ class9(loop_input)/app.py
@@ -0,0 +1,17 @@
+import sys
+print("line1")
+print("line2")
+print(sys.argv)
+print(type(sys.argv))
+                    #   if we want to take input from console by the help of sys.argv
+                    #  output will be in list
+
+
+
+
+# python app.py "12" "2" "Pakistan" "Emmad"      # input in console
+
+# line1
+# line2
+# ['app.py', '12', '2', 'Pakistan', 'Emmad']
+# class 'list'                                   # Output
